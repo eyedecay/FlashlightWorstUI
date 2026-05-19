90190c9e15996ea0be89dca4bfa18d5d0af95908
JJoelouis2 <alvin.hk.lu@gmail.com>
2026-05-19 13:05:22 -0400

    Implement Ball class in ball.py
    Create Ball class for sprite representation in Pygame.

---
+++ ball.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, number, window):
+        starting_pos = [100, 100]
+        radius = 20
+        ball_speed = [3, -3]
+
+        pygame.draw.circle(window, (255, 0, 0), starting_pos, radius)
+
+
+        # For Later
+        """
+        #Initializes Image
+        self.image = pygame.imagae.load(f"ball{number}.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
+
+        self.rect = self.image.get_rect()
+        self.rect.center = starting_pos
+        """
+
+
